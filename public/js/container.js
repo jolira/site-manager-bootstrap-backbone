a1429eab36564ed0760fa3f42a7d4ec0198fbc32
Joachim Kainz <jfk@jolira.com>
2012-05-17 14:04:11 -0700

    increasing the version number.

--- public/js/container.js
+++ public/js/container.js
@@ -17,7 +17,7 @@
     // Initialize the components
     $(function(){
         var Router = Backbone.Router.extend({});
-        
+
         app.router = new Router();
         app.initializers.forEach(function(initializer) {
             initializer(app, "#main-content");
